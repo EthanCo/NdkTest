d916545a075a7529815ee9141fa71e31960fe4f8
EthanCo <fenghenyu@163.com>
2018-08-15 08:13:30 +0800

    C++ test

--- 08_24_cpp_study/08_24_cpp_study/01.cpp
+++ 08_24_cpp_study/08_24_cpp_study/01.cpp
@@ -467,6 +467,294 @@ void main16(){
 	system("pause");
 }
 
+//构造函数的属性初始化列表
+class Teacher17{
+private:
+	char* name;
+public:
+	Teacher17(char* name){
+		this->name = name;
+		cout << "Teacher17有参构造函数" << endl;
+	}
+
+	~Teacher17(){
+		cout << "Teacher17析构函数" << endl;
+	}
+	char* getName(){
+		return this->name;
+	}
+};
+
+class Student17{
+private:
+	int id;
+	//属性对象
+	//Teacher17 t1;
+	//Teacher17 t1 = Teacher17("miss cang");
+	Teacher17 t1;
+	Teacher17 t2;
+public:
+	/*Student17(int id){
+		this->id = id;
+	}*/
+	Student17(int id, char* t1_n, char* t2_n) : t1(t1_n), t2(t2_n){ //: t1(t1_n) 给对象赋值
+		this->id = id;
+		cout << "Student17有参构造函数" << endl;
+	}
+	~Student17(){
+		cout << "Student17析构函数" << endl;
+	}
+	void myprint(){
+		cout << id << "," << t1.getName() << "," << t2.getName()<< endl;
+	}
+};
+
+void main17(){
+	//Student17 s1(10);
+	Student17 s1(10, "miss bo", "miss bo2");
+	Student17 s2(20, "miss cang", "miss cang2");
+	s1.myprint();
+	s2.myprint();
+}
+
+//动态内存分配
+//C++ 通过new(delete) 动态内存分配
+//C      malloc(free)
+
+class Teacher18{
+private:
+	char* name;
+public:
+	Teacher18(char* name){
+		this->name = name;
+		cout << "有参构造函数" << endl;
+	}
+
+	~Teacher18(){
+		cout << "析构函数" << endl;
+	}
+	char* getName(){
+		return this->name;
+	}
+
+	void setName(char* name){
+		this->name = name;
+	}
+
+	//static 静态属性
+	static int total;
+
+	//satic 静态函数
+	static void count(){
+		total++;
+	}
+};
+
+void main18(){
+	//C++
+	Teacher18 *t1 = new Teacher18("jack"); //new是动态内存分配，是放在堆内存上面的
+	cout << t1->getName() << endl;
+	//释放
+	delete t1;
+
+	//C
+	Teacher18 *t2 = (Teacher18*)malloc(sizeof(Teacher18));
+	t2->setName("jack2");
+	cout << t2->getName() << endl;
+	free(t2);
+}
+
+//数组类型
+void main19(){
+	//C
+	int *p1 = (int*)malloc(sizeof(int)* 10);
+	p1[0] = 9;
+	free(p1);
+
+	//C++
+	int *p2 = new int[10];
+	p2[0] = 2;
+	//释放数组 [] 
+	delete[] p2; //free也行，但是不会调用析构函数
+
+	system("pause");
+}
+
+//静态属性初始化赋值
+int Teacher18::total = 9;
+
+void main20(){
+	//Teacher18 t1();
+	Teacher18::total++;
+	cout << Teacher18::total << endl;
+
+	Teacher18::count();
+	//命名空间::Teacher18::count();
+	//外部命名空间::内部命名空间::Teacher18::count();
+	cout << Teacher18::total << endl;
+
+	//直接通过类名访问
+	Teacher18 t1("yuehan");
+	t1.count();
+	cout << t1.total << endl;
+}
+
+//类的大小
+class A{
+public :
+	int i;
+	int j;
+	int k;
+};
+
+class B{
+public:
+	int i;
+	int j;
+	int k;
+	void myprintf(){
+		cout << "打印" << endl;
+	}
+};
+
+void main21(){
+	cout << sizeof(A) << endl; //输出12
+	cout << sizeof(B) << endl; //输出12
+
+	//C/C++ 内存分区:栈、堆、全局(静态、全局)、常量区(字符串)、程序代码区
+	//普通属性与结构体相同的内存布局
+
+	system("pause");
+}
+
+//this是当前对象的指针
+//函数是共享的，必须要有能够标识当前对象是谁的方法
+class Teacher22{
+private:
+	char* name;
+	int age;
+public:
+	Teacher22(char* name, int age){
+		this->name = name;
+		this->age = age;
+		cout << "Teacher22有参构造函数" << endl;
+	}
+	~Teacher22(){
+		cout << "Teacher22析构函数" << endl;
+	}
+	void myprint(){
+		printf("%#x\n", this);
+		cout << this->name << "," << this->age << endl;
+	}
+
+	//常函数，修饰的是this
+	//既不能改变指针的值，又不能改变指针指向的内容
+	//const 修饰函数，内部this相关的属性都无法被修改
+	void mytest() const{
+		//this->name = "yuehan";
+	}
+};
+
+void main22(){
+	Teacher22 t1("jack", 20);
+	printf("%#x\n", &t1);
+	t1.myprint();
+
+
+	Teacher22 t2("rose", 18);
+	printf("%#x\n",&t2);
+	t2.myprint();
+}
+
+//友元函数
+class Class_A{
+	//友元类，可以访问Class_A所有的成员
+	friend class Class_B;
+private:
+	int i;
+public:
+	Class_A(int i){
+		this->i = i;
+	}
+	void myprint(){
+		cout << i << endl;
+	}
+
+	//友元函数的实现，在友元函数中可以访问私有属性
+	friend void modify_i(Class_A*p, int a){
+		p->i = a;
+	}
+};
+
+class Class_B{
+public:
+	//Class_B这个友元类可以访问Class_A类的任何成员
+	void accessAny(){
+		a.i = 10;
+	}
+private:
+	Class_A a;
+};
+
+void main23(){
+	Class_A* a = new Class_A(10);
+	a->myprint();
+
+	modify_i(a,20);
+	a->myprint();
+}
+
+//运算符重载
+//本质上还是函数调用
+class Point{
+private:
+	int x;
+	int y;
+public:
+	Point(int x = 0, int y = 0){
+		this->x = x;
+		this->y = y;
+	}
+
+	//成员函数，运算符重载
+	Point operator- ( Point &p2){
+		Point temp(this->x - p2.x, this->y - p2.y);
+		return temp;
+	}
+
+	void myprint(){
+		cout << x << "," << y << endl;
+	}
+
+	int getX(){
+		return this->x;
+	}
+
+	int getY(){
+		return this->y;
+	}
+};
+
+//重载+号
+Point operator+ (Point &p1,Point &p2){
+	Point temp(p1.getX() + p2.getX(), p1.getY() + p2.getY());
+	return temp;
+}
+
+void main24(){
+	Point p1(10, 20);
+	Point p2(20, 10);
+	Point p3 = p1 + p2;
+
+	p3.myprint();
+
+	Point p4 = p1.operator-(p2);
+	p4.myprint();
+
+	//Java 中 Str 相加也是运算符重载
+	//cout << "" << endl; 也是运算符重载
+}
+
 void main(){
 	//main1();
 	//main2();
@@ -483,7 +771,15 @@ void main(){
 	//main13();
 	//main14();
 	//main15();
-	main16();
+	//main16();
+	//main17();
+	//main18();
+	//main19();
+	//main20();
+	//main21();
+	//main22();
+	//main23();
+	main24();
 
 	system("pause");
 }
