f636b3265921e7d9b333baf2c7251f69bb70df1e
EthanCo <fenghenyu@163.com>
2019-06-04 07:10:38 +0800

    FFmpeg 视频解码

--- NDK_V2/MyWangyiPlayer/app/src/main/cpp/native-lib.cpp
+++ NDK_V2/MyWangyiPlayer/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <unistd.h>
 
 extern "C" {
+//封装格式
+#include "libavformat/avformat.h"
+//解码
 #include "libavcodec/avcodec.h"
-#include <libavformat/avformat.h>
-#include <libswscale/swscale.h>
+//缩放
+#include "libswscale/swscale.h"
+//重采样
+#include "libswresample/swresample.h"
+#include <libavutil/imgutils.h>
+#include <android/native_window_jni.h>
 }
 
 extern "C"
@@ -19,6 +27,7 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_heiko_mywangyiplayer_Player_native_1start(JNIEnv *env, jobject instance, jstring path_,
                                                    jobject surface) {
+    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
     const char *path = env->GetStringUTFChars(path_, 0);
     //FFmpeg 视频绘制 音频绘制
     //初始化网络模块
@@ -26,7 +35,7 @@ Java_com_heiko_mywangyiplayer_Player_native_1start(JNIEnv *env, jobject instance
     //声明上下文 (总上下文)
     AVFormatContext *formatContext = avformat_alloc_context();
 
-    AVDictionary * opts = NULL;
+    AVDictionary *opts = NULL;
     av_dict_set(&opts, "timeout", "3000000", 0); //3秒内打不开，就默认打开失败
     int ret = avformat_open_input(&formatContext, path, NULL, &opts);
     if (ret) {
@@ -65,16 +74,50 @@ Java_com_heiko_mywangyiplayer_Player_native_1start(JNIEnv *env, jobject instance
      * 重视质量:gauss,bilinear
      * 重视锐度:cubic,spline,lanczos
      */
-    SwsContext * sws = sws_getContext(codecContext->width, codecContext->height, codecContext->pix_fmt,
-                   codecContext->width, codecContext->height, AV_PIX_FMT_RGBA, SWS_BILINEAR, 0, 0,
-                   0);
+    SwsContext *sws = sws_getContext(codecContext->width, codecContext->height,
+                                     codecContext->pix_fmt,
+                                     codecContext->width, codecContext->height, AV_PIX_FMT_RGBA,
+                                     SWS_BILINEAR, 0, 0,
+                                     0);
+    ANativeWindow_setBuffersGeometry(nativeWindow, codecContext->width, codecContext->height,
+                                     WINDOW_FORMAT_RGBA_8888);
+    ANativeWindow_Buffer outBuffer;
     while (av_read_frame(formatContext, packet) >= 0) {
         avcodec_send_packet(codecContext, packet);
         AVFrame *frame = av_frame_alloc();
         ret = avcodec_receive_frame(codecContext, frame);
         if (ret == AVERROR(EAGAIN)) {
             continue;
+        } else if (ret < 0) {
+            break;
         }
+        //接收的容器
+        uint8_t *dst_data[4];
+        //每一行的首地址
+        int dst_linesize[0];
+        av_image_alloc(dst_data, dst_linesize, codecContext->width, codecContext->height,
+                       AV_PIX_FMT_RGBA, 1);
+        //绘制
+        sws_scale(sws, frame->data, frame->linesize, 0, frame->height, dst_data, dst_linesize);
+
+        ANativeWindow_lock(nativeWindow, &outBuffer, NULL);
+
+        //渲染
+        uint8_t *firstWindown = static_cast<uint8_t *>(outBuffer.bits);
+        //输入源(rgb)的
+        uint8_t *src_data = dst_data[0];
+        //拿到一行有多少个字节RGBA
+        int destStride = outBuffer.stride * 4;
+        int src_linesize = dst_linesize[0];
+        for (int i = 0; i < outBuffer.height; ++i) {
+            //内存拷贝 来进行渲染
+            memccpy(firstWindown + i * destStride, src_data + i * src_linesize, destStride, i);
+        }
+
+        ANativeWindow_unlockAndPost(nativeWindow);
+        usleep(1000 * 16);
+        av_frame_free(&frame);
+
     }
 
 
--- NDK_V2/MyWangyiPlayer/app/src/main/java/com/heiko/mywangyiplayer/MainActivity.java
+++ NDK_V2/MyWangyiPlayer/app/src/main/java/com/heiko/mywangyiplayer/MainActivity.java
@@ -1,16 +1,18 @@
 package com.heiko.mywangyiplayer;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     private SurfaceView surfaceView;
@@ -23,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1220);
+
         surfaceView = findViewById(R.id.surfaceView);
 
         player = new Player();
@@ -30,7 +37,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void open(View view) {
-        String input = new File(Environment.getExternalStorageDirectory(),"input.mp3").getAbsolutePath();
+        Toast.makeText(getApplicationContext(), "open", Toast.LENGTH_SHORT).show();
+        String input = new File(Environment.getExternalStorageDirectory(), "input.mp4").getAbsolutePath();
+        File file = new File(input);
+        if (!file.exists()) {
+            Toast.makeText(MainActivity.this, "文件不存在", Toast.LENGTH_SHORT).show();
+            return;
+        }
         player.start(input);
     }
 }
